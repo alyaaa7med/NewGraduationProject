4d5ec7437ee7bd2d4deea7135fd597d5a4dbd779
Alyaa Ahmed <alyaaahmed0643@gmail.com>
2024-04-23 08:20:56 +0200

    remove swagger docs

--- sightsaver/urls.py
+++ sightsaver/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path , include
 from django.conf.urls.static import static
 from django.conf import settings
-from drf_spectacular.views import SpectacularAPIView , SpectacularSwaggerView 
+# from drf_spectacular.views import SpectacularAPIView , SpectacularSwaggerView 
 
 
 urlpatterns = [
@@ -26,8 +26,8 @@ urlpatterns = [
     path('accounts/',include('accounts.urls')),
     path('payment/',include('payment.urls')),
     path('reservations/',include('reservations.urls')),
-    path('api/schema',SpectacularAPIView.as_view(),name = "schema"),
-    path('api/docs',SpectacularSwaggerView.as_view(url_name = "schema"))
+    # path('api/schema',SpectacularAPIView.as_view(),name = "schema"),
+    # path('api/docs',SpectacularSwaggerView.as_view(url_name = "schema"))
     
  ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
