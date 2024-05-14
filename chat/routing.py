8a11f5351b6231d77c5114883ea71f970223f89c
Abdelkhaliq Gamal <149975737+abdelkhaliq152@users.noreply.github.com>
2024-05-14 22:48:49 +0300

    Update routing.py

--- chat/routing.py
+++ chat/routing.py
@@ -1,14 +1,14 @@
-# from channels.routing import ProtocolTypeRouter, URLRouter
-# # import app.routing
-# from django.urls import re_path
-# from .consumers import TextRoomConsumer
+from channels.routing import ProtocolTypeRouter, URLRouter
+# import app.routing
+from django.urls import re_path
+from .consumers import TextRoomConsumer
 
-# from django.urls import path
+from django.urls import path
 
 
-# # Here, "" is routing to the URL PersonalChatConsumer which
-# # will handle the chat functionality.
+# Here, "" is routing to the URL PersonalChatConsumer which
+# will handle the chat functionality.
 
-# websocket_urlpatterns = [
-# 	path('ws/<room_name>/' , TextRoomConsumer.as_asgi()) ,
-# ]
+websocket_urlpatterns = [
+	path('ws/<room_name>/' , TextRoomConsumer.as_asgi()) ,
+]
