54992e3c80e93801d92f025366b6e1018d1688c6
Abdelkhaliq Gamal <149975737+abdelkhaliq152@users.noreply.github.com>
2024-05-19 22:51:35 +0300

    Update asgi.py

--- sightsaver/asgi.py
+++ sightsaver/asgi.py
@@ -10,22 +10,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sightsaver.settings')
 
 application = get_asgi_application()
 
-
-#the websocket will open at 127.0.0.1:8000/ws/<room_name>/
-application = ProtocolTypeRouter({
-    
-    "http" : application,
-    'websocket':
-        URLRouter(
-            websocket_urlpatterns
-        )
-    ,
-})
