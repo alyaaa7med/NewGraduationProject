522ed396758bb10bbdcd1e62c3caa8c9209bde2d
Alyaa Ahmed <alyaaahmed0643@gmail.com>
2024-06-01 04:03:29 +0300

    updating migrations on accounts

--- accounts/migrations/0004_alter_otpcode_otp.py
+++ accounts/migrations/0004_alter_otpcode_otp.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0003_alter_otpcode_otp'),
+        ('accounts', '0002_alter_otpcode_otp'),
     ]
 
     operations = [
